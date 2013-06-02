11caaf818cab692f55e4c78411ef964a298df8af
Stasiu <s_bialoskorski@o2.pl>
2013-06-02 18:45:20 +0200

    Dodano tabelke zarzadzania zadaniami

--- Mpi_web/Mpi_web/Users/Tasks.aspx.designer.cs
+++ Mpi_web/Mpi_web/Users/Tasks.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace Mpi_web.Users {
         protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
         /// DropDownList1 control.
         /// </summary>
         /// <remarks>
@@ -56,15 +65,6 @@ namespace Mpi_web.Users {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label1;
-        
-        /// <summary>
-        /// UpdatePanel1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
         
         /// <summary>
         /// TextBox1 control.
